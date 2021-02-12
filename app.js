165da60c03d8fe6cf6a360429c30a29022a0fe76
AbeinemukamaVicent <48617563+Abeinevincent@users.noreply.github.com>
2021-02-12 08:08:28 -0800

    Updated App.js

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-$(document).ready(function(){
-    $('.counter').each(function(){
-        $(this).prop('.counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function(now){
-                $(this).text(Math.ceil(now));
-            }
-        })
-    })
-})
-
-
-
-
-
 
